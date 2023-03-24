1c3ed911388539684e0b8322b77d59445f58683d
Bártfai Tamás <tamas@bartfai.dev>
2023-03-24 12:11:26 +0100

    Updating test code

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -2,7 +2,8 @@
  '("emacs"
    "make"
    "bash"
+   "grep"
    ;; for the daml installer
-   "openjdk@11.0.15" "curl" "nss-certs" "openssl" "tar" "gzip" "ncurses"
+   "openjdk@11" "curl" "nss-certs" "openssl" "tar" "gzip" "ncurses"
    "coreutils" "git" "sed" "findutils"
    ))
