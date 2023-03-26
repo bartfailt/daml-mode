6d383cb6e5c3ef465f7b0b61f2f19d1fecd8aab4
Bártfai Tamás <tamas@bartfai.dev>
2023-03-26 12:24:32 +0200

    Support displaying script results

--- .guix/container/.emacs.d/init-vanilla.el
+++ .guix/container/.emacs.d/init-vanilla.el
@@ -1,16 +1,16 @@
 (require 'package)
-(push '("melpa" . "https://melpa.org/packages/") package-archives) 
-(package-initialize) 
+(push '("melpa" . "https://melpa.org/packages/") package-archives)
+(package-initialize)
 (dolist (pkg '(lsp-mode haskell-mode package-lint)) 
   (unless (package-installed-p pkg) 
     (unless (assoc pkg package-archive-contents) 
       (package-refresh-contents)) 
-    (package-install pkg))) 
+    (package-install pkg)))
 
 (setq custom-file 
       (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory))
 
-(add-to-list 'load-path "~/daml-mode")
+(add-to-list 'load-path (expand-file-name "~/daml-mode/lisp"))
 
 (require 'daml-mode)
 (require 'daml-lsp)
--- lisp/daml-lsp.el
+++ lisp/daml-lsp.el
@@ -4,11 +4,13 @@
 
 ;;; Commentary:
 
-;; Client definition for lsp-mode. 
+;; Client definition for lsp-mode.
 
 ;;; Code:
 
 (require 'lsp-mode)
+(require 'shr)
+(require 'url-util)
 
 ;;;###autoload
 (defcustom daml-lsp-extra-arguments nil
@@ -20,12 +22,70 @@
   "Customization group for daml-lsp."
   :group 'tools)
 
-(defun daml-ls--suggest-project-root ()
-  (and (memq major-mode '(daml-mode))
-       (when-let (dir (locate-dominating-file default-directory "daml.yaml"))
-         (expand-file-name dir))))
-
 (add-to-list 'lsp-language-id-configuration '(daml-mode . "daml"))
+(add-hook 'kill-buffer-hook #'lsp-daml--virtualResource-kill-hook)
+
+(defun lsp-daml--virtualResource-kill-hook ()
+  (when (string-prefix-p "*daml-script " (buffer-name))
+
+    (message "lsp-daml--virtualResource-kill-hook")
+    (let ((script-uri (substring (buffer-name) 13)))
+      (lsp-notify
+       "textDocument/didClose"
+       (list :textDocument
+             (list :uri script-uri
+                   :languageId ""
+                   :version 0
+                   :text ""))))))
+
+(defun lsp-daml--script-result-buffer-name (script-uri)
+  (format "*daml-script %s" (url-unhex-string script-uri)))
+
+(lsp-defun lsp-daml--show-resource ((&Command :arguments?))
+  "Execute the show daml script action"
+  (interactive)
+  (let* ((script-uri (elt arguments? 1))
+         (result-buf-name (lsp-daml--script-result-buffer-name script-uri)))
+     (lsp-notify
+      "textDocument/didOpen"
+      (list :textDocument
+            (list :uri script-uri
+                  :languageId ""
+                  :version 0
+                  :text "")))
+
+     (if-let ((result-buf (get-buffer result-buf-name)))
+         (with-help-window result-buf
+           (print "Try modifying the script to show the results"))
+       (with-output-to-temp-buffer result-buf-name
+         (print (format "Executing script %s"
+                        (url-unhex-string script-uri)))))))
+
+
+(defun lsp-daml--virtualResource-note (workspace params)
+  (lsp-daml--display-virtualResource (ht-get params "uri") (ht-get params "note")))
+
+(defun lsp-daml--virtualResource-change (workspace params)
+  (lsp-daml--display-virtualResource (ht-get params "uri") (ht-get params "contents")))
+
+(defun lsp-daml--display-virtualResource (uri contents)
+  (let* ((result-buffer-name (lsp-daml--script-result-buffer-name uri))
+         (dom (with-temp-buffer
+                (insert contents)
+                (libxml-parse-html-region (point-min) (point-max)))))
+    (when (get-buffer result-buffer-name)
+      (with-current-buffer (get-buffer-create result-buffer-name)
+        (with-help-window result-buffer-name
+          (erase-buffer)
+          (shr-insert-document dom))))))
+
+(defun lsp-daml--report (workspace params)
+  (let ((value (ht-get params "value")))
+    (message "daml progress... %s"
+             (pcase (ht-get value "kind")
+               ("begin" (ht-get value "title"))
+               ("report" (ht-get value "message"))
+               ("end" "Done processing")))))
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection (append '("daml" "ide") daml-lsp-extra-arguments))
@@ -33,14 +93,14 @@
                   :priority -1
                   :multi-root nil
                   :server-id 'daml-ls
+                  :action-handlers (lsp-ht ("daml.showResource" #'lsp-daml--show-resource))
                   :notification-handlers
                   (lsp-ht
-                   ("daml/keepAlive" 'ignore)
-                   ("daml/workspace/validations" 'ignore)
-                   ("window/progress/start" 'ignore)
-                   ("window/progress/report" 'ignore)
-                   ("window/progress/done" 'ignore)
-                   ("$/progress" 'ignore))))
+                   ("daml/virtualResource/didChange" #'lsp-daml--virtualResource-change)
+                   ("daml/virtualResource/note" #'lsp-daml--virtualResource-note)
+                   ("window/progress/start" #'lsp-daml--report)
+                   ("window/progress/report" #'lsp-daml--report)
+                   ("window/progress/done" #'lsp-daml--report))))
 
 (provide 'daml-lsp)
 ;;; daml-lsp.el ends here
