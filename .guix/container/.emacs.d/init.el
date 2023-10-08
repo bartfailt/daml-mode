a9f8d527f5b3d2dc12f2840c2d89cd270078ce8b
Bártfai Tamás <tamas@bartfai.dev>
2023-10-08 10:42:26 +0200

    Remove guix setup for local testing

--- .guix/container/.emacs.d/init.el
+++
@@ -1,1 +0,0 @@
-init-vanilla.el
--- .guix/manifest.scm
+++
@@ -1,9 +0,0 @@
-(specifications->manifest
- '("emacs"
-   "make"
-   "bash"
-   "grep"
-   ;; for the daml installer
-   "openjdk@11" "curl" "nss-certs" "openssl" "tar" "gzip" "ncurses"
-   "coreutils" "git" "sed" "findutils"
-   ))
